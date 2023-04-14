591d5115837be98b5e846cd2d4c269c4fd2e0376
didi <didiprasetyo360@gmail.com>
2023-04-15 04:34:23 +0700

    add relations: student, violation

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Activity extends Model
 {
     use HasFactory;
 
     protected $table = 'student_violations';
+
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(Student::class, 'students_id');
+    }
+
+    public function violation(): BelongsTo
+    {
+        return $this->belongsTo(Violation::class, 'violations_id');
+    }
+
 }
