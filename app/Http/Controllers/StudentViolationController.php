d706b44e6c302f20c590775301143524de627e49
didi <didiprasetyo360@gmail.com>
2023-04-14 16:51:22 +0700

    update process and return value

--- app/Http/Controllers/StudentViolationController.php
+++ app/Http/Controllers/StudentViolationController.php
@@ -4,63 +4,104 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreStudentViolationRequest;
 use App\Http\Requests\UpdateStudentViolationRequest;
+use App\Models\Student;
 use App\Models\StudentViolation;
+use App\Models\Violation;
+use Carbon\Carbon;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class StudentViolationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function index(): View
     {
-        //
+        $studentViolations = StudentViolation::with(['student', 'student.studentClass', 'reported', 'violation'])
+            ->orderBy('violated_at', 'desc')
+            ->paginate(10);
+
+        return View('student-violation.index', compact('studentViolations'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+        $students = Student::pluck('name', 'id');
+        $violations = Violation::pluck('violation_name', 'id');
+
+        return View('student-violation.create', compact('students', 'violations'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreStudentViolationRequest $request)
+    public function store(StoreStudentViolationRequest $request): RedirectResponse
     {
-        //
+        try {
+            $data = [
+                'students_id' => $request->input('student'),
+                'violations_id' => $request->input('violation'),
+                'reported_by' => auth()->id(),
+                'violated_at' => $request->input('violated-at'),
+            ];
+            StudentViolation::query()->create($data);
+
+            return redirect()
+                ->route('student-violations.index')
+                ->with('success', 'Berhasil menambahkan pelanggaran siswa');
+        } catch (\Exception $exception) {
+            return redirect()
+                ->route('student-violations.index')
+                ->with('fail', 'gagal menambahkan pelanggaran siswa');
+
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(StudentViolation $studentViolation)
+    public function show(StudentViolation $studentViolation): View
     {
-        //
+        return View('student-violation.show', compact('studentViolation'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(StudentViolation $studentViolation)
+    public function edit(StudentViolation $studentViolation): View
     {
-        //
+        $students = Student::pluck('name', 'id');
+        $violations = Violation::pluck('violation_name', 'id');
+
+        $studentViolation->violated_at = Carbon::create($studentViolation->violated_at)->format('Y-m-d');
+
+        return View('student-violation.edit', compact('studentViolation', 'students', 'violations'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateStudentViolationRequest $request, StudentViolation $studentViolation)
+    public function update(UpdateStudentViolationRequest $request, StudentViolation $studentViolation): RedirectResponse
     {
-        //
+        try {
+            $data = [
+                'students_id' => $request->input('student'),
+                'violations_id' => $request->input('violation'),
+                'reported_id' => auth()->id(),
+                'violated_at' => $request->input('violated-at'),
+            ];
+            $studentViolation->update($data);
+
+            return redirect()
+                ->route('student-violations.index')
+                ->with('success', 'Berhasil mengubah pelanggaran siswa');
+        } catch (\Exception $exception) {
+            return redirect()
+                ->route('student-violations.index')
+                ->with('success', 'Berhasil mengubah pelanggaran siswa');
+
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(StudentViolation $studentViolation)
     {
-        //
+        try {
+            $studentViolation->delete();
+
+            return redirect()
+                ->route('student-violations.index')
+                ->with('success', 'Berhasil menghapus pelanggaran siswa');
+        } catch (\Exception $exception) {
+            return redirect()
+                ->route('student-violations.index')
+                ->with('success', 'Berhasil menghapus pelanggaran siswa');
+
+        }
     }
 }
