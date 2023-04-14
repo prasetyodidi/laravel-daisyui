f9f716c94715087de242d2a4c94a4f810f7a7205
didi <didiprasetyo360@gmail.com>
2023-04-14 16:18:02 +0700

    update guarded field

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,33 +16,13 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles, HasUuids, SoftDeletes, MustVerifyEmail;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'username',
-        'password',
-    ];
+    protected $guarded = ['id'];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
@@ -57,12 +37,4 @@ class User extends Authenticatable
         return $this->hasMany(StudentViolation::class, 'reported_by', 'id');
     }
 
-    public function canAccessFilament(): bool
-    {
-        return $this->hasVerifiedEmail();
-    }
-
-    public function getFilamentName(): string
-    {
-        return $this->name;
-    }}
+}
