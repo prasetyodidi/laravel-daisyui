fcef76d68a21b96c3be2647e240ea065342dd7e5
didi <didiprasetyo360@gmail.com>
2023-04-15 00:44:41 +0700

    update authorization and rules

--- app/Http/Requests/UpdateViolationTypeRequest.php
+++ app/Http/Requests/UpdateViolationTypeRequest.php
@@ -11,7 +11,7 @@ class UpdateViolationTypeRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return $this->user()->can('update', $this->route('violation_type'));
     }
 
     /**
@@ -22,7 +22,7 @@ class UpdateViolationTypeRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'violation-type-name' => 'required'
         ];
     }
 }
