6ea8a2856c9bcc97c8b68329a9be3a05e09e6bc3
didi <didiprasetyo360@gmail.com>
2023-04-15 05:34:55 +0700

    add backdrop blur

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="drawer-side">
     <label for="my-drawer-2" class="drawer-overlay"></label>
-    <div class="overflow-y-hidden w-80 h-full bg-base-200 text-base-content menu">
-        <div class="fixed w-full z-10 p-5 border-base-200 opacity-30 border-b-2 backdrop-blur-sm">
+    <div class="relative overflow-y-hidden w-80 h-full bg-base-200 text-base-content menu">
+        <div class="w-full fixed top-0 left-0 z-20 p-4 border-base-200/30 border-b-2 backdrop-blur-sm">
             <h1 class="text-primary text-2xl">
                 {{ $title }}
             </h1>
