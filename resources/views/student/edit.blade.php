d956f186b56e154facee1092c7648b46c7a292d4
didi <didiprasetyo360@gmail.com>
2023-04-14 16:36:33 +0700

    create view of edit student

---
+++ resources/views/student/edit.blade.php
@@ -0,0 +1,63 @@
+@extends('dashboard.main')
+
+@section('content')
+    <h1 class="text-xl mb-4">Ubah Siswa</h1>
+
+    <form action="{{ route('students.update', $student->id) }}" method="post">
+        @csrf
+        @method('put')
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+            <label>
+                <input type="text"
+                       name="name"
+                       placeholder="Nama"
+                       value="{{ old('name', $student->name) }}"
+                       class="input input-bordered input-primary w-full"/>
+                <x-validation-message name="name"/>
+            </label>
+            <label>
+                <select name="student-class" class="select select-bordered w-full">
+                    <option disabled>Pilih kelas</option>
+                    @foreach($classes as $class)
+                        @if($class->id == $student->classes_id)
+                            <option value="{{ $class->id }}" selected>{{ $class->class_name }}</option>
+                        @else
+                            <option value="{{ $class->id }}">{{ $class->class_name }}</option>
+                        @endif
+                    @endforeach
+                </select>
+                <x-validation-message name="student-class"/>
+            </label>
+            <label>
+                <input type="text"
+                       name="student-id-number"
+                       placeholder="NIS"
+                       value="{{ old('student-id-number', $student->student_id_number) }}"
+                       class="input input-bordered input-primary w-full"/>
+                <x-validation-message name="student-id-number"/>
+            </label>
+            <label>
+                <input type="text"
+                       name="address"
+                       placeholder="Alamat"
+                       value="{{ old('address', $student->address) }}"
+                       class="input input-bordered input-primary w-full"/>
+                <x-validation-message name="address"/>
+            </label>
+            <label>
+                <select name="gender" class="select select-bordered w-full">
+                    <option disabled>Pilih Jenis Kelamin</option>
+                    @foreach($genders as $gender)
+                        @if($gender['id'] == $student->gender)
+                            <option value="{{ $gender['id'] }}" selected>{{ $gender['name']  }}</option>
+                        @else
+                            <option value="{{ $gender['id'] }}">{{ $gender['name']  }}</option>
+                        @endif
+                    @endforeach
+                </select>
+                <x-validation-message name="gender"/>
+            </label>
+        </div>
+        <button type="submit" class="btn btn-primary">Ubah</button>
+    </form>
+@endsection
