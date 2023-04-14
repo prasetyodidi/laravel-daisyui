cc978fefd2c2d4779db4402dc7b48f21219fb6fd
didi <didiprasetyo360@gmail.com>
2023-04-14 16:50:50 +0700

    update process and return value

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,65 +2,118 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\Gender;
 use App\Http\Requests\StoreStudentRequest;
 use App\Http\Requests\UpdateStudentRequest;
 use App\Models\Student;
+use App\Models\StudentClass;
+use Exception;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class StudentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function index(): View
     {
-        //
+        $students = Student::query()->with('studentClass')->paginate(10);
+
+        return View('student.index', compact('students'));
+
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function create(): View
     {
-        //
+        $classes = StudentClass::all();
+
+        $genders = [
+            ['id' => Gender::Man, 'name' => 'Laki - laki'],
+            ['id' => Gender::Women, 'name' => 'Perempuan'],
+        ];
+
+        return View('student.create', compact('classes', 'genders'));
+
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreStudentRequest $request)
+    public function store(StoreStudentRequest $request): RedirectResponse
     {
-        //
+        try {
+
+            $data = [
+                'name' => $request->input('name'),
+                'classes_id' => $request->input('student-class'),
+                'student_id_number' => $request->input('student-id-number'),
+                'address' => $request->input('address'),
+                'gender' => $request->input('gender'),
+            ];
+
+            Student::query()->create($data);
+
+            return redirect()->route('student.index')->with('success', 'Berhasil menambahkan data siswa');
+
+
+        } catch (Exception $exception) {
+
+            return redirect()->route('student.index')->with('fail', 'Gagal menambahkan data siswa');
+
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Student $student)
+    public function show(Student $student): View
     {
-        //
+
+        return View('student.show', compact('student'));
+
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Student $student)
+    public function edit(Student $student): View
     {
-        //
+
+        $classes = StudentClass::all();
+
+        $genders = [
+            ['id' => Gender::Man->value, 'name' => 'Laki - laki'],
+            ['id' => Gender::Women->value, 'name' => 'Perempuan'],
+        ];
+
+        return View('student.edit', compact('student', 'classes', 'genders'));
+
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateStudentRequest $request, Student $student)
+    public function update(UpdateStudentRequest $request, Student $student): RedirectResponse
     {
-        //
+        try {
+
+            $data = [
+                'name' => $request->input('name'),
+                'classes_id' => $request->input('student-class'),
+                'student_id_number' => $request->input('student-id-number'),
+                'address' => $request->input('address'),
+                'gender' => $request->input('gender'),
+            ];
+
+            $student->update($data);
+
+            return redirect()->route('students.index')->with('success', 'Berhasil mengubah data siswa');
+
+        } catch (Exception $exception) {
+
+            return redirect()->route('students.index')->with('fail', 'Gagal mengubah data siswa');
+
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Student $student)
+    public function destroy(Student $student): RedirectResponse
     {
-        //
+        try {
+
+            $student->delete();
+
+            return redirect()->route('students.index')->with('success', 'Berhasil menghapus data siswa');
+
+        } catch (Exception $exception) {
+
+            return redirect()->route('students.index')->with('fail', 'Gagal menghapus data siswa');
+
+        }
     }
 }
