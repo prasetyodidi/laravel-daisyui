75e3057ee5753b3457a11d009e9d6a609fd4f5a4
didi <didiprasetyo360@gmail.com>
2023-04-14 16:35:37 +0700

    create view of student classes

---
+++ resources/views/student-class/index.blade.php
@@ -0,0 +1,58 @@
+@extends('dashboard.main')
+
+@section('content')
+    <div class="w-full">
+        <div class="overflow-x-auto w-full">
+            <table class="table w-full" aria-describedby="list of users">
+                <thead>
+                <tr>
+                    <th id="no" class="z-0">No</th>
+                    <th id="class-name">Nama Kelas</th>
+                    <th id="homeroom-teacher">Wali Kelas</th>
+                    <th id="action">Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                    @foreach($studentClasses as $studentClass)
+                    <tr class="hover hover:cursor-pointer">
+                        <th id="row-number">{{ $loop->iteration }}</th>
+                        <td>{{ $studentClass->class_name }}</td>
+                        <td>{{ $studentClass->homeroomTeacher->name }}</td>
+                        <td class="flex flex-row gap-2">
+                            <a href="{{ route('student-classes.edit', $studentClass->id) }}"
+                               class="flex flex-row items-center gap-1 hover:link text-orange-600 text-sm py-1">
+                                <x-heroicon-o-pencil class="h-4 w-4 "/>
+                                edit
+                            </a>
+                            <a href="#modal-delete-{{ $loop->iteration }}"
+                               class="flex flex-row items-center gap-1 hover:link text-red-600 text-sm py-1">
+                                <x-heroicon-o-trash class="h-4 w-4 "/>
+                                delete
+                            </a>
+
+                            <div class="modal" id="modal-delete-{{ $loop->iteration }}">
+                                <div class="modal-box w-5/12 max-w-5xl">
+                                    <form action="{{ route('student-classes.destroy', $studentClass->id) }}"
+                                          method="post">
+                                        @csrf
+                                        @method('delete')
+
+                                        <h1 class="font-bold text-xl text-center">Hapus Kelas</h1>
+                                        <h2 class="text-center">Apakah anda yakin menghapus data kelas ini</h2>
+
+                                        <div class="modal-action flex flex-row justify-between mt-8">
+                                            <a href="#" class="btn">Tutup</a>
+                                            <button type="submit" class="btn btn-error">Hapus</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            {{ $studentClasses->links()  }}
+        </div>
+    </div>
+@endsection
