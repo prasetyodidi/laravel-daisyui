acd9f5a119296e98f92390e5e9932acd31bb067b
didi <didiprasetyo360@gmail.com>
2023-04-14 16:47:09 +0700

    update background color

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,8 +1,10 @@
 <div class="drawer-side">
     <label for="my-drawer-2" class="drawer-overlay"></label>
-    <div class="overflow-y-hidden w-80 h-full bg-base-100 text-base-content menu">
-        <div class="fixed w-full bg-base-100 z-10 p-5 border-base-200 border-2">
-            {{ $title }}
+    <div class="overflow-y-hidden w-80 h-full bg-base-200 text-base-content menu">
+        <div class="fixed w-full z-10 p-5 border-base-200 opacity-30 border-b-2 backdrop-blur-sm">
+            <h1 class="text-primary text-2xl">
+                {{ $title }}
+            </h1>
         </div>
         {{ $menu }}
     </div>
