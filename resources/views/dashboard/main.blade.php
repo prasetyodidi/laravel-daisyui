0450757deef4ec9ce4070e81c45bf18e7e1c37c5
didi <didiprasetyo360@gmail.com>
2023-04-14 16:46:22 +0700

    update menu

--- resources/views/dashboard/main.blade.php
+++ resources/views/dashboard/main.blade.php
@@ -15,44 +15,134 @@
 
             <x-navbar/>
 
-            @yield('content')
+            <div x-data="{ notification: true }" class="z-30 toast toast-top toast-end">
+                @if(session('success'))
+                <div x-show="notification" class="alert alert-success">
+                    <div>
+                        <x-heroicon-o-x-circle @click="notification = false" class="h-6 w-6 hover:cursor-pointer"/>
+                        <span>{{ session('success') }}</span>
+                    </div>
+                </div>
+                @endif
+                @if(session('fail'))
+                <div x-show="notification" class="alert alert-error">
+                    <div>
+                        <x-heroicon-o-x-circle @click="notification = false" class="h-6 w-6 hover:cursor-pointer"/>
+                        <span>{{ session('fail') }}</span>
+                    </div>
+                </div>
+                @endif
+            </div>
+
+            <div class="w-full py-20 px-8">
+
+                @yield('content')
+
+            </div>
 
         </div>
 
         <x-sidebar>
-            <x-slot name="title">DaisyUI</x-slot>
+            <x-slot name="title">Point Siswa</x-slot>
             <x-slot name="menu">
-                <ul class="overflow-y-auto p-4 mt-14">
+                <ul class="overflow-y-auto p-4 pt-20 text-sm">
                     <li>
-                        <a>
+                        <a class="menu-item">
                             <x-heroicon-o-home class="h-6 w-6"/>
                             Dashboard
                         </a>
                     </li>
-                    <li><a>Aktivity</a></li>
-                    <li><a>Ketentuan Point</a></li>
-                    <li><a>Kelas</a></li>
-                    <li><a>Siswa</a></li>
-                    <li><a>Laporan Pelanggaran</a></li>
-                    <li><a>Pelanggaran Siswa</a></li>
-                    <li><a>Guru</a></li>
-                    <li><a>Pelanggaran</a></li>
-                    <li><a>Jenis Pelanggaran</a></li>
+                    <li>
+                        <a class="menu-item">
+                            <x-heroicon-o-document-text class="h-5 w-5"/>
+                            Aktivity
+                        </a>
+                    </li>
+                    <x-sidebar-item>
+                        <x-slot name="title">Ketentuan Point</x-slot>
+                        <x-slot name="icon">
+                            <x-heroicon-o-document-text class="h-5 w-5"/>
+                        </x-slot>
+                        <x-slot name="routeName">point-conditions.index</x-slot>
+                    </x-sidebar-item>
+                    @can('viewAny', \App\Models\StudentClass::class)
+                    <x-sidebar-item>
+                        <x-slot name="title">Kelas</x-slot>
+                        <x-slot name="icon">
+                            <x-heroicon-o-document-text class="h-5 w-5"/>
+                        </x-slot>
+                        <x-slot name="routeName">student-classes.index</x-slot>
+                        <x-slot name="activeAt">student-classes.*</x-slot>
+                    </x-sidebar-item>
+                    @endcan
+                    <x-sidebar-item>
+                        <x-slot name="title">Siswa</x-slot>
+                        <x-slot name="icon">
+                            <x-heroicon-o-document-text class="h-5 w-5"/>
+                        </x-slot>
+                        <x-slot name="routeName">students.index</x-slot>
+                        <x-slot name="activeAt">students.*</x-slot>
+                    </x-sidebar-item>
+                    <x-sidebar-item>
+                        <x-slot name="title">Pelanggaran Siswa</x-slot>
+                        <x-slot name="icon">
+                            <x-heroicon-o-document-text class="h-5 w-5"/>
+                        </x-slot>
+                        <x-slot name="routeName">student-violations.index</x-slot>
+                    </x-sidebar-item>
+                    <x-sidebar-item>
+                        <x-slot name="title">Guru</x-slot>
+                        <x-slot name="icon">
+                            <x-heroicon-o-document-text class="h-5 w-5"/>
+                        </x-slot>
+                        <x-slot name="routeName">teacher.index</x-slot>
+                    </x-sidebar-item>
+                    <x-sidebar-item>
+                        <x-slot name="title">Pelanggaran</x-slot>
+                        <x-slot name="icon">
+                            <x-heroicon-o-document-text class="h-5 w-5"/>
+                        </x-slot>
+                        <x-slot name="routeName">violations.index</x-slot>
+                    </x-sidebar-item>
+                    <x-sidebar-item>
+                        <x-slot name="title">Jenis Pelanggaran</x-slot>
+                        <x-slot name="icon">
+                            <x-heroicon-o-document-text class="h-5 w-5"/>
+                        </x-slot>
+                        <x-slot name="routeName">violation-types.index</x-slot>
+                    </x-sidebar-item>
+                    <x-sidebar-item>
+                        <x-slot name="title">Penghargaan</x-slot>
+                        <x-slot name="icon">
+                            <x-heroicon-o-document-text class="h-5 w-5"/>
+                        </x-slot>
+                        <x-slot name="routeName">achievements.index</x-slot>
+                    </x-sidebar-item>
+                    <x-sidebar-item>
+                        <x-slot name="title">Jenis Penghargaaan</x-slot>
+                        <x-slot name="icon">
+                            <x-heroicon-o-document-text class="h-5 w-5"/>
+                        </x-slot>
+                        <x-slot name="routeName">achievement-types.index</x-slot>
+                    </x-sidebar-item>
                     <x-my-dropdown>
                         <x-slot name="title">Permission</x-slot>
                         <x-slot name="content">
                             <ul>
                                 <li>
                                     <a href="{{ route('users.index') }}"
-                                       class="{{ request()->routeIs('users.index') ? 'active' : '' }}">User</a>
+                                       class="{{ request()->routeIs('users.index') ? 'active' : '' }}
+                                               menu-item">
+                                        User
+                                    </a>
                                 </li>
                                 <li>
                                     <a href="{{ route('permissions.index') }}"
-                                       class="{{ request()->routeIs('permissions.index') ? 'active' : '' }}">
+                                       class="{{ request()->routeIs('permissions.index') ? 'active' : '' }} menu-item">
                                         Permission
                                     </a>
                                 </li>
-                                <li><a>Role</a></li>
+                                <li><a class="menu-item">Role</a></li>
                             </ul>
                         </x-slot>
                     </x-my-dropdown>
