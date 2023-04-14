95f63a50e28dd1c33e94ec515142e0afd37e8cbd
didi <didiprasetyo360@gmail.com>
2023-04-14 16:34:35 +0700

    create view of student violations

---
+++ resources/views/student-violation/index.blade.php
@@ -0,0 +1,98 @@
+@extends('dashboard.main')
+
+@section('content')
+    <div class="w-full">
+
+        <div class="flex flex-row justify-end">
+            <a href="{{ route('student-violations.create')  }}" class="btn btn-primary my-2">Tambah</a>
+        </div>
+        <div class="overflow-x-auto w-full">
+            <table class="table w-full" aria-describedby="list of users">
+                <thead>
+                <tr>
+                    <th id="no" class="z-0">No</th>
+                    <th id="name">Nama</th>
+                    <th id="email">Kelas</th>
+                    <th id="role">Pelapor</th>
+                    <th id="verified-at">Pelanggaran</th>
+                    <th id="verified-at">Point</th>
+                    <th id="verified-at">Tanggal Pelanggaran</th>
+                    <th id="action">Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($studentViolations as $student)
+                    <tr class="hover hover:cursor-pointer">
+                        <th id="row-number">{{ $loop->iteration }}</th>
+                        <td>{{ $student->student->name }}</td>
+                        <td>{{ $student->student->studentClass->class_name }}</td>
+                        <td>{{ $student->reported->name }}</td>
+                        <td>{{ $student->violation->violation_name }}</td>
+                        <td>{{ $student->student->name }}</td>
+                        <td>{{ $student->violated_at }}</td>
+                        <td class="flex flex-row gap-2 h-full">
+                            <a href="{{ route('student-violations.edit', $student->id) }}"
+                               class="flex flex-row items-center gap-1 hover:link text-orange-600 text-sm py-1">
+                                <x-heroicon-o-pencil class="h-4 w-4 "/>
+                                edit
+                            </a>
+                            <a href="#modal-delete-{{ $loop->iteration }}"
+                               class="flex flex-row items-center gap-1 hover:link text-red-600 text-sm py-1">
+                                <x-heroicon-o-trash class="h-4 w-4 "/>
+                                delete
+                            </a>
+
+                            <div class="modal" id="modal-delete-{{ $loop->iteration }}">
+                                <div class="modal-box w-5/12 max-w-5xl">
+                                    <form action="{{ route('student-violations.destroy', $student->id) }}"
+                                          method="post">
+                                        @csrf
+                                        @method('delete')
+
+                                        <h1 class="font-bold text-xl text-center">Hapus Pelanggaran Siswa</h1>
+                                        <h2 class="text-center">
+                                            Apakah anda yakin menghapus data pelanggaran siswa ini
+                                        </h2>
+
+                                        <div class="modal-action flex flex-row justify-between mt-8">
+                                            <a href="#" class="btn">Tutup</a>
+                                            <button type="submit" class="btn btn-error">Hapus</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+            {{ $studentViolations->links()  }}
+        </div>
+    </div>
+
+    <div class="modal" id="modal-import-student">
+        <form action="{{ route('user.import') }}" method="post" enctype="multipart/form-data">
+            @csrf
+            @method('post')
+            <div class="modal-box">
+                <h1 class="font-bold text-lg mb-6">Import User</h1>
+
+                <div class="form-control w-full max-w-xs">
+                    <input type="file"
+                           name="excel-file"
+                           id="excel-file"
+                           class="file-input file-input-bordered w-full max-w-xs"/>
+                    <label class="label">
+                        <x-validation-message name="excel-file"/>
+                    </label>
+                </div>
+
+                <div class="modal-action flex flex-row justify-between">
+                    <a href="#" class="btn">Close</a>
+                    <a href="#" class="btn btn-primary">Import</a>
+                </div>
+            </div>
+        </form>
+    </div>
+
+@endsection
