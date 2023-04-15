03877cdde17f841097015ed28573d9cb52f98d36
didi <didiprasetyo360@gmail.com>
2023-04-15 17:19:03 +0700

    update resource authorization and process

--- app/Http/Controllers/AchievementController.php
+++ app/Http/Controllers/AchievementController.php
@@ -5,62 +5,87 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreAchievementRequest;
 use App\Http\Requests\UpdateAchievementRequest;
 use App\Models\Achievement;
+use App\Models\AchievementType;
+use Exception;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class AchievementController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+
+    public function __construct()
     {
-        //
+        $this->authorizeResource(Achievement::class);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function index(): View
     {
-        //
+        $achievements = Achievement::with('achievementType')->orderBy('created_at', 'desc')->paginate(10);
+
+        return View('achievement.index', compact('achievements'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreAchievementRequest $request)
+    public function create(): View
     {
-        //
+        $achievementTypes = AchievementType::pluck('achievement_type_name', 'id');
+
+        return View('achievement.create', compact('achievementTypes'));
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Achievement $achievement)
+    public function store(StoreAchievementRequest $request): RedirectResponse
     {
-        //
+        try {
+            $data = [
+                'achievement_name' => $request->input('achievement-name'),
+                'achievement_point' => $request->input('achievement-point'),
+                'achievement_types_id' => $request->input('achievement-type'),
+            ];
+
+            Achievement::query()->create($data);
+
+            return redirect()->route('achievements.index')->with('success', 'Berhasil menambahkan pencapaian baru');
+        } catch (Exception $exception) {
+            return redirect()->route('achievements.index')->with('fail', 'Gagal menambahkan pencapaian baru');
+        }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Achievement $achievement)
+    public function show(Achievement $achievement): View
     {
-        //
+        return View('achievement.show', compact('achievement'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateAchievementRequest $request, Achievement $achievement)
+    public function edit(Achievement $achievement): View
     {
-        //
+        $achievementTypes = AchievementType::pluck('achievement_type_name', 'id');
+
+        return View('achievement.edit', compact('achievement', 'achievementTypes'));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Achievement $achievement)
+    public function update(UpdateAchievementRequest $request, Achievement $achievement): RedirectResponse
     {
-        //
+        try {
+            $data = [
+                'achievement_name' => $request->input('achievement-name'),
+                'achievement_point' => $request->input('achievement-point'),
+                'achievement_types_id' => $request->input('achievement-type'),
+            ];
+
+            $achievement->update($data);
+
+            return redirect()->route('achievements.index')->with('success', 'Berhasil mengubah pencapaian');
+        } catch (Exception $exception) {
+            return redirect()->route('achievements.index')->with('fail', 'Gagal mengubah pencapaian');
+        }
+    }
+
+    public function destroy(Achievement $achievement): RedirectResponse
+    {
+        try {
+            $achievement->delete();
+
+            return redirect()->route('achievements.index')->with('success', 'Berhasil menghapus pencapaian');
+        } catch (Exception $exception) {
+            return redirect()->route('achievements.index')->with('fail', 'Gagal menghapus pencapaian');
+        }
     }
 }
