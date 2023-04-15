2caa3599fc9b749851b02874de37707ab8b78f71
didi <didiprasetyo360@gmail.com>
2023-04-15 17:15:53 +0700

    update student achievement resource view

---
+++ resources/views/student-achievement/create.blade.php
@@ -0,0 +1,45 @@
+@extends('dashboard.main')
+
+@section('content')
+    <h1 class="text-xl mb-4">Tambah</h1>
+
+    <form action="{{ route('student-achievements.store') }}" method="post">
+        @csrf
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+            <label>
+                <select name="student" class="select select-bordered w-full">
+                    <option disabled selected>Pilih Siswa</option>
+                    @foreach($students as $key => $student)
+                        @if($key == old('student'))
+                            <option value="{{ $key }}" selected>{{ $student }}</option>
+                        @else
+                            <option value="{{ $key }}">{{ $student }}</option>
+                        @endif
+                    @endforeach
+                </select>
+                <x-validation-message name="student"/>
+            </label>
+            <label>
+                <select name="achievement" class="select select-bordered w-full">
+                    <option disabled selected>Pilih Pencapaian</option>
+                    @foreach($achievements as $key => $achievement)
+                        @if($key == old('achievement'))
+                            <option value="{{ $key }}" selected>{{ $achievement  }}</option>
+                        @else
+                            <option value="{{ $key }}">{{ $achievement  }}</option>
+                        @endif
+                    @endforeach
+                </select>
+                <x-validation-message name="achievement"/>
+            </label>
+            <label>
+                <input type="date"
+                       name="achieved-at"
+                       value="{{ old('achieved-at') }}"
+                       class="input input-bordered input-primary w-full"/>
+                <x-validation-message name="achieved-at"/>
+            </label>
+        </div>
+        <button type="submit" class="btn btn-primary">Tambah</button>
+    </form>
+@endsection
---
+++ resources/views/student-achievement/edit.blade.php
@@ -0,0 +1,46 @@
+@extends('dashboard.main')
+
+@section('content')
+    <h1 class="text-xl mb-4">Ubah</h1>
+
+    <form action="{{ route('student-achievements.update', $studentAchievement->id) }}" method="post">
+        @csrf
+        @method('put')
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+            <label>
+                <select name="student" class="select select-bordered w-full">
+                    <option disabled>Pilih Siswa</option>
+                    @foreach($students as $key => $student)
+                        @if($key == $studentAchievement->students_id)
+                            <option value="{{ $key }}" selected>{{ $student }}</option>
+                        @else
+                            <option value="{{ $key }}">{{ $student }}</option>
+                        @endif
+                    @endforeach
+                </select>
+                <x-validation-message name="student"/>
+            </label>
+            <label>
+                <select name="achievement" class="select select-bordered w-full">
+                    <option disabled>Pilih Pencapaian</option>
+                    @foreach($achievements as $key => $achievement)
+                        @if($key == $studentAchievement->achievements_id)
+                            <option value="{{ $key }}" selected>{{ $achievement }}</option>
+                        @else
+                            <option value="{{ $key }}">{{ $achievement  }}</option>
+                        @endif
+                    @endforeach
+                </select>
+                <x-validation-message name="achievement"/>
+            </label>
+            <label>
+                <input type="date"
+                       name="achieved-at"
+                       value="{{ old('achieved-at', $studentAchievement->achieved_at) }}"
+                       class="input input-bordered input-primary w-full"/>
+                <x-validation-message name="achieved-at"/>
+            </label>
+        </div>
+        <button type="submit" class="btn btn-primary">Ubah</button>
+    </form>
+@endsection
