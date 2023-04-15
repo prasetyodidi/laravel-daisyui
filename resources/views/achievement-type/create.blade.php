cda0a837463359000200a5516df47fbdc20f522b
didi <didiprasetyo360@gmail.com>
2023-04-15 17:15:11 +0700

    update achievement type resource view

---
+++ resources/views/achievement-type/create.blade.php
@@ -0,0 +1,20 @@
+@extends('dashboard.main')
+
+@section('content')
+    <h1 class="text-xl mb-4">Tambah Jenis Pencapaian</h1>
+
+    <form action="{{ route('achievement-types.store') }}" method="post">
+        @csrf
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+            <label>
+                <input type="text"
+                       name="achievement-type-name"
+                       placeholder="Nama Jenis Pencapaian"
+                       value="{{ old('achievement-type-name') }}"
+                       class="input input-bordered input-primary w-full"/>
+                <x-validation-message name="achievement-type-name"/>
+            </label>
+        </div>
+        <button type="submit" class="btn btn-primary">Tambah</button>
+    </form>
+@endsection
