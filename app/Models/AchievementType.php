6eac5f9a0404edd5663bb50cc777000e714082f9
didi <didiprasetyo360@gmail.com>
2023-04-15 17:19:42 +0700

    add guarded and violations relation

--- app/Models/AchievementType.php
+++ app/Models/AchievementType.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class AchievementType extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function violations(): HasMany
+    {
+        return $this->hasMany(Achievement::class, 'achievement_types_id');
+    }
 }
