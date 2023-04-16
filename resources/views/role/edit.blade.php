31eccbb1fbfd8816c242dc449bc32294407e55d6
didi <didiprasetyo360@gmail.com>
2023-04-16 10:48:55 +0700

    checked all

--- resources/views/role/edit.blade.php
+++ resources/views/role/edit.blade.php
@@ -2,8 +2,30 @@
 
 @section('content')
     <div class="w-full pt-20">
-        <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" value="{{ $role->name }}" />
+        <input type="text"
+               placeholder="Type here"
+               value="{{ $role->name }}"
+               class="input input-bordered w-full max-w-xs"/>
 
+        <div>
+            @foreach($permissions as $key => $permission)
+                <div class="flex flex-col" x-data="{ isAllChecked: true }">
+                    <label>
+                        <input type="checkbox" :checked="isAllChecked" @click="isAllChecked = !isAllChecked">
+                        {{ $key }}
+                    </label>
+                    <div class="pl-3 flex flex-col">
+                        @foreach($permission as $action)
+                            <label>
+                                <input type="checkbox" :checked="isAllChecked">
+                                {{ $action }}
+                            </label>
+                        @endforeach
+                    </div>
+                </div>
+            @endforeach
+        </div>
 
     </div>
 @endsection
+
