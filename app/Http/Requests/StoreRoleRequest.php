857ca431d7bfba697ed77c2dea31b1db04550444
didi <didiprasetyo360@gmail.com>
2023-04-16 10:51:06 +0700

    update rules and authorization

--- app/Http/Requests/StoreRoleRequest.php
+++ app/Http/Requests/StoreRoleRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Role;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreRoleRequest extends FormRequest
@@ -11,7 +12,7 @@ class StoreRoleRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return $this->user()->can('store', Role::class);
     }
 
     /**
--- app/Http/Requests/UpdateRoleRequest.php
+++ app/Http/Requests/UpdateRoleRequest.php
@@ -11,7 +11,7 @@ class UpdateRoleRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return $this->user()->can('update', $this->route('role'));
     }
 
     /**
