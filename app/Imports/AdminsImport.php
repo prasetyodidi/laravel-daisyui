a17c4c8e42c979420d991efea06024aadd0a9053
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-04-18 23:12:50 +0700

    create admin: model, factory, seeder, controller and form request

---
+++ app/Imports/AdminsImport.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\User;
+use Maatwebsite\Excel\Concerns\ToModel;
+
+class AdminsImport implements ToModel
+{
+    
+    public function model(array $row)
+    {
+        return new User([
+            //
+        ]);
+    }
+}
