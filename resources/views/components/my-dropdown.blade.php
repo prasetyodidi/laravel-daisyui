e1104ef650abb914a64f2cc8b37dbe8ec1529ebd
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-04-27 11:14:18 +0700

    add transition

--- resources/views/components/my-dropdown.blade.php
+++ resources/views/components/my-dropdown.blade.php
@@ -1,10 +1,18 @@
-<div x-data="{ open : true }" class="menu bg-base-100">
-    <div class="flex flex-row justify-between">
-        <a class="px-4">{{ $title }}</a>
+<div x-data="{ open : true }"
+     class="menu bg-base-100 my-2 py-2 rounded-md">
+    <div class="flex flex-row justify-between px-2">
+        <a>{{ $title }}</a>
         <x-heroicon-o-chevron-down x-show="!open" @click="open = true" class="w-6 h-6 hover:cursor-pointer"/>
         <x-heroicon-o-chevron-up x-show="open" @click="open = false" class="w-6 h-6 hover:cursor-pointer"/>
     </div>
-    <div x-show="open" class="flex flex-col">
+    <div x-show="open"
+         x-transition:enter="transition ease-out duration-200"
+         x-transition:enter-start="transform opacity-0 scale-95"
+         x-transition:enter-end="transform opacity-100 scale-100"
+         x-transition:leave="transition ease-in duration-75"
+         x-transition:leave-start="transform opacity-100 scale-100"
+         x-transition:leave-end="transform opacity-0 scale-95"
+         class="flex flex-col">
         {{ $content }}
     </div>
 </div>
