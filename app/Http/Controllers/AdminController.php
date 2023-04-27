821b64f885285198b6839ae7ad22953a66a68011
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-04-27 11:19:01 +0700

    update resource functions

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,62 +5,84 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreAdminRequest;
 use App\Http\Requests\UpdateAdminRequest;
 use App\Models\Admin;
+use App\Models\User;
+use Exception;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class AdminController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+
+    public function __construct()
     {
-        //
+        $this->authorizeResource(Admin::class);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function index(): View
     {
-        //
+        $admins = User::role('admin')->paginate(10);
+
+        return View('admin.index', compact('admins'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreAdminRequest $request)
+    public function create(): View
     {
-        //
+        return View('admin.create');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Admin $admin)
+    public function store(StoreAdminRequest $request): RedirectResponse
     {
-        //
+        try {
+            $data = [
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'username' => $request->input('username'),
+                'password' => bcrypt($request->input('password')),
+                'employee_id_number' => $request->input('employee-id-number'),
+            ];
+
+            $admin = User::query()->create($data);
+            $admin->assignRole('admin');
+
+            return redirect()->route('admins.index')->with('success', 'Berhasil menambahkan admin');
+        } catch (Exception $exception) {
+            return redirect()->route('admins.index')->with('fail', 'Gagal menambahkan admin');
+        }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Admin $admin)
+    public function show(Admin $admin): View
     {
-        //
+        return View('admin.show', compact('admin'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateAdminRequest $request, Admin $admin)
+    public function edit(Admin $admin): View
     {
-        //
+        return View('admin.edit', compact('admin'));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Admin $admin)
+    public function update(UpdateAdminRequest $request, Admin $admin): RedirectResponse
     {
-        //
+        try {
+            $data = [
+                'name' => $request->input('name'),
+                'username' => $request->input('username'),
+                'employee_id_number' => $request->input('employee-id-number'),
+            ];
+            $admin->update($data);
+
+            return redirect()->route('admins.index')->with('success', 'Berhasil mengubah data admin');
+        } catch (Exception $exception) {
+            return redirect()->route('admins.index')->with('fail', 'Gagal mengubah data admin');
+        }
+    }
+
+    public function destroy(Admin $admin): RedirectResponse
+    {
+        try {
+            $admin->delete();
+            return redirect()->route('admins.index')->with('success', 'Berhasil menghapus data admin');
+        } catch (Exception $exception) {
+            return redirect()->route('admins.index')->with('success', 'Gagal menghapus data admin');
+        }
     }
 }
