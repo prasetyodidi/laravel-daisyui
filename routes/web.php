9577e9a256b6c8188b8c7a913c3b706325f8cd99
Didi Prasetyo <didiprasetyo360@gmail.com>
2023-06-23 10:48:30 +0700

    delete route register & set default route register

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index'])
